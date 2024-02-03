976c6bb2d8b0e16f7160c20ca45e22a0b0b1e8fd
Chaitanyagit-2005 <2022bit041@sggs.ac.in>
2024-02-03 22:41:26 +0530

    [200~Develop a functionality to get the count of valid registration numbers from the given array. A valid registration number follows the format: YYYYDDDSSS, where:

---
+++ ProgrammingAssignment1.java
@@ -0,0 +1,66 @@
+class SY2022bit041 {
+
+    public static void main(String[] args) {
+        // Test your logic here
+        String[] registrations = {};
+        SY2022bit041 cm = new SY2022bit041();
+        int validCount = cm.getValidRegistrationsCount(registrations);
+        System.out.println(validCount);
+    }
+
+    public int getValidRegistrationsCount(String[] registrations) {
+       // Invalid input
+        if (registrations == null) {
+            return -1; 
+        }
+
+        int validCountRegNo = 0;
+        for (String regNo : registrations) {
+            if (isValidRegistration(regNo)) {
+                validCountRegNo++;
+            }
+        }
+        return validCountRegNo;
+    }
+
+    public boolean isValidRegistration(String inputRegNo) {
+        if (inputRegNo.length() != 10) {
+            return false;
+        }
+
+        int admissionYear = Integer.parseInt(inputRegNo.substring(0, 4));
+        String inputbranchCode = inputRegNo.substring(4, 7);
+
+        
+        if ((isValidYear(admissionYear)) &&
+                (isValidBranch(inputbranchCode)) && (isValidSerialNo(inputRegNo))) {
+           
+            return true;
+        }
+
+        return false;
+    }
+    
+ //Checks if the year is between 2000 and 2024
+    public boolean isValidYear(int year) {
+        return year >= 2000 && year < 2024;
+    }
+    
+    /*Checks if the brnchCode is valid or not
+     Valid departments: bit, bcs, bme, bch */
+    public boolean isValidBranch(String branchCode) {
+        return branchCode.equalsIgnoreCase("bit") || branchCode.equalsIgnoreCase("bcs")
+                || branchCode.equalsIgnoreCase("bme") || branchCode.equalsIgnoreCase("bch");
+    }
+    
+    //Checks whether the 3 digit serial number contains all integer digits
+    public boolean isValidSerialNo(String inputRegNo){
+            for (int i = 7; i < inputRegNo.length(); i++) {
+                if (!Character.isDigit(inputRegNo.charAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+    }
+}
+
